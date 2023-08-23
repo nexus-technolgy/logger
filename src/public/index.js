9b3fc02a1d6c3abf4ab9d6981925aee85a2c8482
Daniel Bryar <daniel.bryar@beemit.com.au>
2023-08-23 19:01:20 +1000

    revert: remove esbuild bundling

--- src/public/index.js
+++
@@ -1,4 +0,0 @@
-var Logger = require("./class/logger.js");
-var logger = require("./function/logger.js");
-
-module.exports = { Logger, logger };
--- src/public/index.mjs
+++
@@ -1,4 +0,0 @@
-import Logger from "./class/logger.js";
-import logger from "./function/logger.js";
-
-export { Logger, logger };
